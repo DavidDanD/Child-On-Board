8b384ec3a5b98d3f311a62a808a8f8a09f253eec
DolevVaknin <49777821+DolevVaknin@users.noreply.github.com>
2021-05-20 13:00:34 +0300

    Add FSR and gyro readings

--- cloud_publish/cloud_publish.ino
+++ cloud_publish/cloud_publish.ino
@@ -36,10 +36,33 @@ String apiKeyValue = "tPmAT5Ab3j7F9";
 
 #define DHT_PIN 15
 #define DHT_TYPE DHT11
+#define FSR_PIN 34
 
 //DHT
 float h,t,hic;
 DHT dht(DHT_PIN, DHT_TYPE);
+
+//MPU
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050
+int16_t disAccX, disAccY, disAccZ, disGyroX, disGyroY, disGyroZ;
+int squareDistanceAcc, squareDistanceGyro;
+
+//gyro
+int16_t gyroX, gyroY, gyroZ;
+int16_t accX, accY, accZ;
+int16_t lastGyroX, lastGyroY, lastGyroZ;
+int16_t lastAccX, lastAccY, lastAccZ;
+
+//Gyroscope sensor deviation
+float gyroXThreshold = 0.4;
+float gyroYThreshold = 0.4;
+float gyroZThreshold = 0.4;
+float accXThreshold = 0.4;
+float accYThreshold = 0.4;
+float accZThreshold = 0.4;
+
+//FSR
+int fsrValue = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -54,7 +77,65 @@ void setup() {
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
 
+  Wire.begin(21, 22, 100000); // sda, scl
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.println("Setup complete");
+
   dht.begin();
+}
+
+void getreadings(){
+//  mpu.getEvent(&a, &g, &temp);
+//
+  lastAccX = accX;
+  lastAccY = accY;
+  lastAccZ = accZ;
+
+  lastGyroX = gyroX;
+  lastGyroY = gyroY;
+  lastGyroZ = gyroZ;
+  
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(true);
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.requestFrom((uint16_t)MPU_ADDR, (uint8_t)14, true); // request a total of 14 registers
+  accX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  accY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  accZ= Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+//  mpuTemperature = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  gyroX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  gyroY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  gyroZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  
+  // Read humidity
+  h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  t = dht.readTemperature();
+  // Check if temperature read failed.
+  
+  if (isnan(t) || isnan(h)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  
+  // Compute heat index in Celsius (isFahreheit = false)
+  hic = dht.computeHeatIndex(t, h, false);
+  
+  disAccX = lastAccX-accX;
+  disAccY = lastAccY-accY;
+  disAccZ = lastAccZ-accZ;
+  disGyroX = lastGyroX-gyroX;
+  disGyroY = lastGyroY-gyroY;
+  disGyroZ = lastGyroZ-gyroZ;
+
+  squareDistanceAcc = disAccX*disAccX + disAccY*disAccY + disAccZ*disAccZ;
+  squareDistanceGyro =  disGyroX*disGyroX + disGyroY*disGyroY + disGyroZ*disGyroZ;
+
+  fsrValue = analogRead(FSR_PIN);
 }
 
 void loop() {
@@ -68,23 +149,12 @@ void loop() {
     // Specify content-type header
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    // Read humidity
-    h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
-    t = dht.readTemperature();
-    // Check if temperature read failed.
-  
-    if (isnan(t) || isnan(h)) {
-      Serial.println(F("Failed to read from DHT sensor!"));
-      return;
-    }
-  
-    // Compute heat index in Celsius (isFahreheit = false)
-    hic = dht.computeHeatIndex(t, h, false);
+    getreadings();
     
     // Prepare your HTTP POST request data
-    String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(t)
-                           + "&value2=" + String(h) + "&value3=" + String(hic) + "&value4=" + String("Blattt") + "";
+    String httpRequestData = "api_key=" + apiKeyValue + "&temperature=" + String(t)
+                           + "&weight=" + String(fsrValue) + "&gyro=" + String(squareDistanceGyro)
+                           + "&accelerometer=" + String(squareDistanceAcc) + "";
 
     Serial.print("httpRequestData: ");
     Serial.println(httpRequestData);
